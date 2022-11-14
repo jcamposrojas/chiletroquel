5217871b842d87f764f104549a6f31e298a962ab
Juan I. Campos R <jicampos@devmobile.cl>
2022-11-14 18:50:07 -0300

    Upd Nomina: Corrige Bug en retorno de dias ausentismo

--- l10n_cl_hr/model/hr_holidays.py
+++ l10n_cl_hr/model/hr_holidays.py
@@ -15,6 +15,9 @@ class HRHolidays(models.Model):
         #En el caso de las licencias descontamos dias corridos
         if employee_id and self.holiday_status_id.is_continued:
             time_delta = date_to - date_from
-            return math.ceil(time_delta.days + float(time_delta.seconds) / 86400)
+            dias = math.ceil(time_delta.days + float(time_delta.seconds) / 86400)
+            # JCR. No ocupa campo hours
+            hours = 0
+            return {'days': days, 'hours': hours}
         else:
             return super(HRHolidays, self)._get_number_of_days(date_from, date_to, employee_id)
